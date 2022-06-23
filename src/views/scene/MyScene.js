63415077b7fe859d4265691babb95115945f8ca9
Malek <chtiouimal@gmail.com>
2022-06-23 15:33:14 +0100

    new

--- src/views/scene/MyScene.js
+++ src/views/scene/MyScene.js
@@ -4,7 +4,7 @@ import * as THREE from "three";
 import MyModel from "../../components/model/MyModel";
 import { musicCollection } from "../../utils/constants/MusicCollections";
 
-export default function MyScene(props) {
+export default function MyScene({ setPlaying }) {
   const url =
     "https://firebasestorage.googleapis.com/v0/b/music-gallery-da2d1.appspot.com/o/Backgrounds%2FMuseum-1.png?alt=media&token=724ccd1e-1647-4325-9888-1ee8e46014ae";
 
@@ -15,7 +15,7 @@ export default function MyScene(props) {
   const data = musicCollection;
 
   useEffect(() => {
-    ready ? props.setPlaying(true) : props.setPlaying(false);
+    ready ? setPlaying(true) : setPlaying(false);
   }, [ready]);
 
   return (
